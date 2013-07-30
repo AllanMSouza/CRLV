d20c86729fcc86d0b718f6165f7700077079e58a
Allan <allan@ShiTless.(none)>
2013-07-30 15:33:28 -0300

    Inicializar Cartão [done]

--- src/compsi/crlv/controller/ControllerSmartCard.java
+++ src/compsi/crlv/controller/ControllerSmartCard.java
@@ -6,14 +6,32 @@ package compsi.crlv.controller;
 
 import compsi.crlv.model.SmartCard;
 import compsi.crlv.view.MainWondow;
+import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import javax.smartcardio.CardException;
 import javax.swing.JOptionPane;
+import pkcs11_lea.constants.Constants;
 import pkcs11_lea.util.Hex;
 
 /**
@@ -24,35 +42,349 @@ public class ControllerSmartCard implements ActionListener {
 
     private SmartCard sc;
     private MainWondow mw;
+    private String result;
 
     public ControllerSmartCard(MainWondow m) {
         sc = new SmartCard();    
         mw = m;
         
         mw.getMiVersao().addActionListener(this);
+        mw.getMiInicializarCartao().addActionListener(this);
     }    
     
     @Override
     public void actionPerformed(ActionEvent ae) {
-        try {
-            //throw new UnsupportedOperationException("Not supported yet.");
-            String result = getVersion();
-            System.out.println(result);
-            JOptionPane.showMessageDialog(null, "Versão: " + result);
-        } catch (CardException ex) {
-            Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+        String op = ae.getActionCommand();
+        
+        switch(op){
+            case "Inicializar Cartão":
+                    try {
+                        result = inicializaCartao();
+                        ShowError("Inicializar Cartão", result);
+                    } catch (CardException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NoSuchPaddingException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NoSuchAlgorithmException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvalidKeyException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvalidKeySpecException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalBlockSizeException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (BadPaddingException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvalidAlgorithmParameterException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                break;
+                
+            case "Versão":
+                    try {
+                        result = getVersion();
+                        JOptionPane.showMessageDialog(null, "Versão: "+result);
+                    } catch (CardException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (FileNotFoundException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(ControllerSmartCard.class.getName()).log(Level.SEVERE, null, ex);
+                    }
         }
     }
     
-    private String getVersion() throws CardException, FileNotFoundException, IOException{
+    private String getVersion() throws 
+            CardException, 
+            FileNotFoundException, 
+            IOException {
         PCSCManager.LOGAR("Obter a versão do Applet");
         sc.setResponse(PCSCManager.sendAPDU("8007000000"));
         sc.setResultado(Hex.printBytesHexa(sc.getResponse()));
         return sc.getResultado().substring(0, sc.getResultado().length() - 4 );        
     }
+    
+    private String inicializaCartao() throws CardException,
+            FileNotFoundException,
+            IOException,
+            NoSuchPaddingException,
+            NoSuchAlgorithmException,
+            InvalidKeyException,
+            InvalidKeySpecException,
+            IllegalBlockSizeException,
+            BadPaddingException,
+            InvalidAlgorithmParameterException {
+
+        PCSCManager.LOGAR("Inicializa_Cartao");
+
+        sc.setPuk("");
+        while (sc.getPuk().length() != 8) {
+            sc.setPuk(JOptionPane.showInputDialog("Digite o PUK com oito caracteres"));
+            if (sc.getPuk() == null) {
+                break;
+            }
+            if (sc.getPuk().length() != 8) {
+                sc.setPuk(JOptionPane.showInputDialog("Digite o PUK com oito caracteres"));
+                sc.setPuk(null);
+            }
+        }
+
+        if (sc.getPuk() == null) {
+            return "0C00";
+        }
+
+        sc.setPukAscii(sc.getPuk().getBytes("US-ASCII"));
+        Cursor hourglassCursor = new Cursor(Cursor.WAIT_CURSOR);
+        mw.setCursor(hourglassCursor);
+
+        // GET AUTH PUBLIC KEY
+
+        sc.setResponse(PCSCManager.sendAPDU("8056000000"));
+        sc.setResultado(Hex.printBytesHexa(sc.getResponse()));
+        sc.setResult(sc.getResultado().substring(sc.getResultado().length() - 4, sc.getResultado().length() - 2));
+
+        if ((sc.getResult().equals(Constants.RESPONSE_APDU_OK))) {
+            PCSCManager.LOGAR("ENVIAR CHAVE AES PARTE 1");
+            sc.setModulo(sc.getResultado().substring(0, sc.getResultado().length() - 4));
+
+            sc.setN(new BigInteger(sc.getModulo(), 16));
+            sc.setE(new BigInteger("010001", 16));
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPublicKeySpec pubKeySpec = new RSAPublicKeySpec(sc.getN(), sc.getE());
+            RSAPublicKey publicKey = (RSAPublicKey) keyFactory.generatePublic(pubKeySpec);
+
+
+            byte[] key_AES = new byte[16];
+            SecureRandom r = new SecureRandom();
+            r.nextBytes(key_AES);
+
+            Provider cp = Security.getProvider("SunJCE");
+            Cipher c = Cipher.getInstance("RSA/ECB/OAEPwithSHA1andMGF1Padding", cp);
+            c.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] enc = c.doFinal(key_AES);
+            String enc_s = Hex.printBytesHexa(enc);
+            String parte1 = enc_s.substring(0, 478);
+            int len = parte1.length();
+            String parte2 = enc_s.substring(478, 512);
+            len = parte2.length();
+            
+            sc.setResponse(PCSCManager.sendAPDU("80570000EF" + parte1));
+            sc.setResultado(Hex.printBytesHexa(sc.getResponse()));
+            sc.setResult(sc.getResultado().substring(sc.getResultado().length() - 4, sc.getResultado().length() - 2));
+            if ((sc.getResult().equals(Constants.RESPONSE_APDU_OK))) {
+                PCSCManager.LOGAR("ENVIAR CHAVE AES PARTE 2");
+                sc.setResponse(PCSCManager.sendAPDU("8057800011" + parte2));
+                sc.setResultado(Hex.printBytesHexa(sc.getResponse()));
+                sc.setResult(sc.getResultado().substring(sc.getResultado().length() - 4, sc.getResultado().length() - 2));
+                if ((sc.getResult().equals(Constants.RESPONSE_APDU_OK))) {
+                    PCSCManager.LOGAR("PEGAR DESAFIO E ENVIAR PUK");
+
+                    sc.setDesafioEnc(sc.getResultado().substring(0, sc.getResultado().length() - 4));
+                    sc.setDesafioByte(Hex.toByteHexArray(sc.getDesafioEnc()));
+                    
+                    String iv = "0000000000000000";
+                    javax.crypto.spec.IvParameterSpec ivspec = new javax.crypto.spec.IvParameterSpec(iv.getBytes());
+                    SecretKeySpec secretKeySpec = new SecretKeySpec(key_AES, "AES");
+
+                    // Instantiate the cipher
+                    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+                    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivspec);
+                    
+                    sc.setOriginal(cipher.doFinal(sc.getDesafioByte()));
+                    sc.setOriginalS(Hex.printBytesHexa(sc.getOriginal()));
+
+                    sc.setHostInfo(sc.getOriginalS() + Hex.printBytesHexa(sc.getPukAscii()) + "0000000000000000");
+                    sc.setHostInfoByte(Hex.toByteHexArray(sc.getHostInfo()));
+
+                    cipher = Cipher.getInstance("AES/CBC/NoPadding");
+                    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivspec);
+                    sc.setDestino(cipher.doFinal(sc.getHostInfoByte()));
+                    sc.setDestinoS(Hex.printBytesHexa(sc.getDestino()));
+                    
+                    sc.setResponse(PCSCManager.sendAPDU("8057810020" + sc.getDestinoS()));
+                    sc.setResultado(Hex.printBytesHexa(sc.getResponse()));
+
+                }
+            }
+        }
+
+        Cursor normalCursor = new Cursor(Cursor.DEFAULT_CURSOR);
+        mw.setCursor(normalCursor);
+
+        return sc.getResultado().substring(sc.getResultado().length() - 4, sc.getResultado().length());
+    } // FIM INICIALIZACAO
+    
+    void ShowError(String rotina, String rv) {
+        JOptionPane.showMessageDialog(null, "Função: " + rotina);
+
+        Integer rv1 = Integer.parseInt(rv.substring(0, 2), 16);
+        Integer rv2 = Integer.parseInt(rv.substring(2, 4), 16);
+
+        switch (rv1) {
+
+            case 0x000001:
+                JOptionPane.showMessageDialog(null, "ERRO DE CONEXAO\n");
+                break;
+            case 0x000002:
+                JOptionPane.showMessageDialog(null, "ERRO AO SELECIONAR O APPLET LSIDOC\n");
+                break;
+            case 0x000003:
+                JOptionPane.showMessageDialog(null, "ERRO NA GERACAO DE NUMEROS RANDOMICOS\n");
+                break;
+            case 0x000004:
+                JOptionPane.showMessageDialog(null, "TAMANHO DIFERENTE DE 258 BYTES\n");
+                break;
+            case 0x000005:
+                JOptionPane.showMessageDialog(null, "ERRO DO BLOCO OAEP\n");
+                break;
+            case 0x000006:
+                JOptionPane.showMessageDialog(null, "TAMANHO DIFERENTE DE 18 BYTES\n");
+                break;
+            case 0x000007:
+                JOptionPane.showMessageDialog(null, "ERRO AO DECRIPTAR AES128\n");
+                break;
+            case 0x000008:
+                JOptionPane.showMessageDialog(null, "ERRO AO ENCRIPTAR AES128\n");
+                break;
+            case 0x000009:
+                JOptionPane.showMessageDialog(null, "ERRO NOME DO ARQUIVO INEXISTENTE\n");
+                break;
+            case 0x00000A:
+                JOptionPane.showMessageDialog(null, "ERRO SEM ESPACO DE MEMORIA\n");
+                break;
+            case 0x00000B:
+                JOptionPane.showMessageDialog(null, "ERRO DE GRAVACAO DO ARQUIVO\n");
+                break;
+            case 0x00000C:
+                JOptionPane.showMessageDialog(null, "OPERAÇÃO CANCELADA !!!! \n");
+                break;
+            case 0x00000D:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x00000E:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x00000F:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x000010:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x000011:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x000012:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x000013:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+            case 0x000014:
+                JOptionPane.showMessageDialog(null, "\n");
+                break;
+
+
+            case 0x000090:
+                JOptionPane.showMessageDialog(null, "OPERAÇÃO REALIZADA COM SUCESSO !!!! OK\n");
+                break;
+            case 0x000067:
+                JOptionPane.showMessageDialog(null, "TAMANHO INVALIDO\n");
+                break;
+            case 0x00006D:
+                JOptionPane.showMessageDialog(null, "CODIGO DE INSTRUCAO INVALIDO OU NAO SUPORTADO\n");
+                break;
+            case 0x00006E:
+                JOptionPane.showMessageDialog(null, "CLASSE NAO SUPORTADA\n");
+                break;
+            case 0x00006F:
+                JOptionPane.showMessageDialog(null, "DIAGNOSTICO NAO PRECISO\n");
+                break;
+            case 0x000064:
+                JOptionPane.showMessageDialog(null, "TROCAR PUK COM PUK\n");
+                break;
+
+            case 0x00006A: {
+                switch (rv2) {
+                    case 0x000080:
+                        JOptionPane.showMessageDialog(null, "PRESENCA DE DADOS INVALIDOS\n");
+                        break;
+                    case 0x000081:
+                        JOptionPane.showMessageDialog(null, "FUNCAO NAO SUPORTADA, PARAMETROS P1/P2 INVALIDOS\n");
+                        break;
+                    case 0x000088:
+                        JOptionPane.showMessageDialog(null, "DADOS DE REFERENCIA NAO ENCONTRADOS\n");
+                        break;
+                    case 0x000086:
+                        JOptionPane.showMessageDialog(null, "PARAMETROS P1/P2 INVALIDOS\n");
+                        break;
+                    case 0x000082:
+                        JOptionPane.showMessageDialog(null, "NÃO HÁ DOCUMENTO ARMAZENADO NESTE CONTAINER\n");
+                        break;
+                }
+                break;
+            }
+
+
+            case 0x000069: {
+                switch (rv2) {
+                    case 0x000082:
+                        JOptionPane.showMessageDialog(null, "ESTADO DE SEGURANCA NAO SATISFEITO\n");
+                        break;
+                    case 0x000083:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK BLOQUEADO OU INVALIDO\n");
+                        break;
+                    case 0x000085:
+                        JOptionPane.showMessageDialog(null, "KID INCORRETO\n");
+                        break;
+                }
+                break;
+            }
+
+            case 0x000063: {
+                switch (rv2) {
+                    case 0x000000C0:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK BLOQUEADOS\n");
+                        break;
+                    case 0x000000C1:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 01 TENTATIVA RESTANTE\n");
+                        break;
+                    case 0x000000C2:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 02 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C3:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 03 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C4:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 04 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C5:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 05 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C6:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 06 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C7:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 07 TENTATIVAS RESTANTES\n");
+                        break;
+                    case 0x000000C8:
+                        JOptionPane.showMessageDialog(null, "PIN OU PUK INCORRETO, 08 TENTATIVAS RESTANTES\n");
+                        break;
+                }
+                break;
+            }
+            default:
+                JOptionPane.showMessageDialog(null, "INDEFINIDO!!!\n");
+                break;
+        }
+
+    }
+
+    
+    
     
 }
--- src/compsi/crlv/model/SmartCard.java
+++ src/compsi/crlv/model/SmartCard.java
@@ -6,6 +6,7 @@ package compsi.crlv.model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.math.BigInteger;
 
 /**
  *
@@ -46,6 +47,201 @@ public class SmartCard {
         byte[] oldResponse = this.response;
         this.response = response;
         propertyChangeSupport.firePropertyChange(PROP_RESPONSE, oldResponse, response);
+    }
+
+        private String modulo;
+    public static final String PROP_MODULO = "modulo";
+
+    public String getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(String modulo) {
+        String oldModulo = this.modulo;
+        this.modulo = modulo;
+        propertyChangeSupport.firePropertyChange(PROP_MODULO, oldModulo, modulo);
+    }
+
+        private String result;
+    public static final String PROP_RESULT = "result";
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        String oldResult = this.result;
+        this.result = result;
+        propertyChangeSupport.firePropertyChange(PROP_RESULT, oldResult, result);
+    }
+
+        private BigInteger n;
+    public static final String PROP_N = "n";
+
+    public BigInteger getN() {
+        return n;
+    }
+
+    public void setN(BigInteger n) {
+        BigInteger oldN = this.n;
+        this.n = n;
+        propertyChangeSupport.firePropertyChange(PROP_N, oldN, n);
+    }
+
+        private BigInteger e;
+    public static final String PROP_E = "e";
+
+    public BigInteger getE() {
+        return e;
+    }
+
+    public void setE(BigInteger e) {
+        BigInteger oldE = this.e;
+        this.e = e;
+        propertyChangeSupport.firePropertyChange(PROP_E, oldE, e);
+    }
+
+        private String puk;
+    public static final String PROP_PUK = "puk";
+
+    public String getPuk() {
+        return puk;
+    }
+
+    public void setPuk(String puk) {
+        String oldPuk = this.puk;
+        this.puk = puk;
+        propertyChangeSupport.firePropertyChange(PROP_PUK, oldPuk, puk);
+    }
+
+        private byte[] pukAscii;
+    public static final String PROP_PUKASCII = "pukAscii";
+
+    public byte[] getPukAscii() {
+        return pukAscii;
+    }
+
+    public void setPukAscii(byte[] pukAscii) {
+        byte[] oldPukAscii = this.pukAscii;
+        this.pukAscii = pukAscii;
+        propertyChangeSupport.firePropertyChange(PROP_PUKASCII, oldPukAscii, pukAscii);
+    }
+
+        private String desafioEnc;
+    public static final String PROP_DESAFIOENC = "desafioEnc";
+
+    public String getDesafioEnc() {
+        return desafioEnc;
+    }
+
+    public void setDesafioEnc(String desafioEnc) {
+        String oldDesafioEnc = this.desafioEnc;
+        this.desafioEnc = desafioEnc;
+        propertyChangeSupport.firePropertyChange(PROP_DESAFIOENC, oldDesafioEnc, desafioEnc);
+    }
+
+        private byte[] desafioByte;
+    public static final String PROP_DESAFIOBYTE = "desafioByte";
+
+    public byte[] getDesafioByte() {
+        return desafioByte;
+    }
+
+    public void setDesafioByte(byte[] desafioByte) {
+        byte[] oldDesafioByte = this.desafioByte;
+        this.desafioByte = desafioByte;
+        propertyChangeSupport.firePropertyChange(PROP_DESAFIOBYTE, oldDesafioByte, desafioByte);
+    }
+
+        private String iv;
+    public static final String PROP_IV = "iv";
+
+    public String getIv() {
+        return iv;
+    }
+
+    public void setIv(String iv) {
+        String oldIv = this.iv;
+        this.iv = iv;
+        propertyChangeSupport.firePropertyChange(PROP_IV, oldIv, iv);
+    }
+
+        private byte[] original;
+    public static final String PROP_ORIGINAL = "original";
+
+    public byte[] getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(byte[] original) {
+        byte[] oldOriginal = this.original;
+        this.original = original;
+        propertyChangeSupport.firePropertyChange(PROP_ORIGINAL, oldOriginal, original);
+    }
+
+        private String originalS;
+    public static final String PROP_ORIGINALS = "originalS";
+
+    public String getOriginalS() {
+        return originalS;
+    }
+
+    public void setOriginalS(String originalS) {
+        String oldOriginalS = this.originalS;
+        this.originalS = originalS;
+        propertyChangeSupport.firePropertyChange(PROP_ORIGINALS, oldOriginalS, originalS);
+    }
+
+        private String hostInfo;
+    public static final String PROP_HOSTINFO = "hostInfo";
+
+    public String getHostInfo() {
+        return hostInfo;
+    }
+
+    public void setHostInfo(String hostInfo) {
+        String oldHostInfo = this.hostInfo;
+        this.hostInfo = hostInfo;
+        propertyChangeSupport.firePropertyChange(PROP_HOSTINFO, oldHostInfo, hostInfo);
+    }
+
+        private byte[] hostInfoByte;
+    public static final String PROP_HOSTINFOBYTE = "hostInfoByte";
+
+    public byte[] getHostInfoByte() {
+        return hostInfoByte;
+    }
+
+    public void setHostInfoByte(byte[] hostInfoByte) {
+        byte[] oldHostInfoByte = this.hostInfoByte;
+        this.hostInfoByte = hostInfoByte;
+        propertyChangeSupport.firePropertyChange(PROP_HOSTINFOBYTE, oldHostInfoByte, hostInfoByte);
+    }
+
+        private byte[] destino;
+    public static final String PROP_DESTINO = "destino";
+
+    public byte[] getDestino() {
+        return destino;
+    }
+
+    public void setDestino(byte[] destino) {
+        byte[] oldDestino = this.destino;
+        this.destino = destino;
+        propertyChangeSupport.firePropertyChange(PROP_DESTINO, oldDestino, destino);
+    }
+
+        private String destinoS;
+    public static final String PROP_DESTINOS = "destinoS";
+
+    public String getDestinoS() {
+        return destinoS;
+    }
+
+    public void setDestinoS(String destinoS) {
+        String oldDestinoS = this.destinoS;
+        this.destinoS = destinoS;
+        propertyChangeSupport.firePropertyChange(PROP_DESTINOS, oldDestinoS, destinoS);
     }
 
 }
